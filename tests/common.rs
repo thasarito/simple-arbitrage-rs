6f6914625ebe3ae8616137b03d45c85c26ac3a20
thasarito <thasarit.pysr@gmail.com>
2022-01-11 23:07:06 +0700

    add deploy function

--- tests/common.rs
+++ tests/common.rs
@@ -1,7 +1,8 @@
 use std::sync::Arc;
 use std::time::Duration;
 
-use ethers::prelude::artifacts::{CompactContract, Contract};
+// use ethers::prelude::artifacts::{CompactContract, Contract};
+use ethers::prelude::artifacts::CompactContract;
 use ethers::prelude::*;
 use ethers::utils::GanacheInstance;
 
@@ -16,4 +17,17 @@ pub fn connect(ganache: &GanacheInstance, idx: usize) -> Arc<Provider<Http>> {
         .interval(Duration::from_millis(10u64))
         .with_sender(sender);
     Arc::new(provider)
+}
+
+pub async fn deploy<T>(
+    compact_contract: CompactContract,
+    client: Arc<Provider<T>>,
+) -> ContractFactory<Provider<T>>
+where
+    T: JsonRpcClient,
+{
+    let (abi, bytes, _) = compact_contract.into_parts_or_default();
+    let factory = ContractFactory::new(abi, bytes, client);
+
+    factory
 }
--- tests/simulation.rs
+++ tests/simulation.rs
@@ -32,22 +32,36 @@ mod simulation_test {
 
         let client1 = connect(&ganache, 0);
 
-        let compactFactory: CompactContract = serde_json::from_str(include_str!(
+        let compact_factory: CompactContract = serde_json::from_str(include_str!(
             "../out/UniswapV2Factory.sol/UniswapV2Factory.json"
         ))
         .unwrap();
-        let (abi, bytes, _) = compactFactory.into_parts_or_default();
-        let factoryFactory = ContractFactory::new(abi, bytes, client1.clone());
-
-        let factoryAddrs = factoryFactory
+        let factory = deploy(compact_factory, client1.clone()).await;
+        let factory = factory
             .deploy(H160::zero())
             .unwrap()
             .legacy()
             .send()
             .await
-            .unwrap()
-            .address();
+            .unwrap();
 
-        dbg!(factoryAddrs);
+        let compact_router: CompactContract = serde_json::from_str(include_str!(
+            "../out/UniswapV2Router.sol/UniswapV2Router02.json"
+        ))
+        .unwrap();
+        let router = deploy(compact_router, client1.clone()).await;
+        let router = router
+            .deploy(factory.address())
+            .unwrap()
+            .legacy()
+            .send()
+            .await
+            .unwrap();
+
+        let compact_pair: CompactContract = serde_json::from_str(include_str!(
+            "../out/UniswapV2Factory.sol/UniswapV2Pair.json"
+        ))
+        .unwrap();
+        let pair = deploy(compact_pair, client1.clone()).await;
     }
 }
