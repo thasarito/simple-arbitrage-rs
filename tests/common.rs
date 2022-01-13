c3a04430fdf095f2698b1a2b7ecee2f52b2e2105
thasarito <thasarit.pysr@gmail.com>
2022-01-13 23:49:33 +0700

    refactor creating pool

--- tests/common.rs
+++ tests/common.rs
@@ -7,6 +7,8 @@ use ethers::prelude::*;
 use ethers::utils::GanacheInstance;
 
 use ethers::providers::Http;
+use forge_test::bindings::t_token::TToken;
+use forge_test::bindings::uniswap_v2_router_02::UniswapV2Router02;
 
 // connects the private key to http://localhost:8545
 pub fn connect(ganache: &GanacheInstance, idx: usize) -> Arc<Provider<Http>> {
@@ -29,4 +31,73 @@ where
     let factory = ContractFactory::new(abi, bytes, client);
 
     factory
+}
+
+pub async fn create_pool<T>(
+    token: H160,
+    weth: H160,
+    amount_token: U256,
+    amount_eth: U256,
+    client: Arc<Provider<T>>,
+) where
+    T: JsonRpcClient,
+{
+    let deployer = client.default_sender().unwrap();
+    let compact_factory: CompactContract =
+        serde_json::from_str(include_str!("../out/UniswapV2Factory.json")).unwrap();
+    let factory = deploy(compact_factory, client.clone()).await;
+    let factory = factory
+        .deploy(H160::zero())
+        .unwrap()
+        .legacy()
+        .send()
+        .await
+        .unwrap();
+
+    let compact_router: CompactContract =
+        serde_json::from_str(include_str!("../out/UniswapV2Router02.json")).unwrap();
+
+    let router = deploy(compact_router, client.clone()).await;
+    let router = router
+        .deploy((factory.address(), weth))
+        .unwrap()
+        .legacy()
+        .send()
+        .await
+        .unwrap();
+
+    let block = client.get_block_number().await.unwrap();
+    let time = client.get_block(block).await.unwrap().unwrap().timestamp;
+
+    let searcher_token = TToken::new(token, client.clone());
+
+    searcher_token
+        .approve(router.address(), U256::MAX)
+        .legacy()
+        .send()
+        .await
+        .unwrap();
+
+    let allowance = searcher_token
+        .allowance(deployer, router.address())
+        .call()
+        .await
+        .unwrap();
+    dbg!(allowance);
+
+    let searcher_router = UniswapV2Router02::new(router.address(), client.clone());
+    searcher_router
+        .add_liquidity_eth(
+            token,
+            amount_token,
+            U256::zero(),
+            U256::zero(),
+            deployer,
+            time * (2u32),
+        )
+        .legacy()
+        .value(amount_eth)
+        .send()
+        .await
+        .unwrap();
 }
--- tests/simulation.rs
+++ tests/simulation.rs
@@ -3,15 +3,11 @@ pub use common::*;
 
 #[cfg(test)]
 mod simulation_test {
-    use std::ops::Mul;
 
     use super::*;
     use ethers::{
-        prelude::{artifacts::CompactContract, Middleware, H160, U256},
+        prelude::{artifacts::CompactContract, U256},
         utils::Ganache,
-    };
-    use forge_test::bindings::{
-        t_token::TToken, uniswap_v2_router_02::UniswapV2Router02, weth9::WETH9,
     };
 
     #[tokio::test]
@@ -23,18 +19,7 @@ mod simulation_test {
 
         let provider = connect(&ganache, 0);
         // let searcher = connect(&ganache, 1);
-
-        let compact_factory: CompactContract =
-            serde_json::from_str(include_str!("../out/UniswapV2Factory.json")).unwrap();
-        let factory = deploy(compact_factory, provider.clone()).await;
-        let factory = factory
-            .deploy(H160::zero())
-            .unwrap()
-            .legacy()
-            .send()
-            .await
-            .unwrap();
-
+        // Start deploy token
         let compact_weth: CompactContract =
             serde_json::from_str(include_str!("../out/WETH9.sol/WETH9.json")).unwrap();
         let weth = deploy(compact_weth, provider.clone()).await;
@@ -44,73 +29,29 @@ mod simulation_test {
             serde_json::from_str(include_str!("../out/TToken.sol/TToken.json")).unwrap();
         let token = deploy(compact_token, provider.clone()).await;
         let token = token.deploy(()).unwrap().legacy().send().await.unwrap();
+        // Finish deploy token
 
-        let compact_router: CompactContract =
-            serde_json::from_str(include_str!("../out/UniswapV2Router02.json")).unwrap();
-
-        let router = deploy(compact_router, provider.clone()).await;
-        let router = router
-            .deploy((factory.address(), weth.address()))
-            .unwrap()
-            .legacy()
-            .send()
-            .await
-            .unwrap();
-
-        let block = provider.get_block_number().await.unwrap();
-        let time = provider.get_block(block).await.unwrap().unwrap().timestamp;
-
-        let searcher_token = TToken::new(token.address(), provider.clone());
-        // let token_balance = searcher_token.balance_of(deployer).call().await.unwrap();
-        // // dbg!(token_balance);
         let token_balance_1 = U256::from_dec_str("26736768576059").unwrap();
         let eth_balance_1 = U256::from_dec_str("9561078446416170138").unwrap();
 
-        searcher_token
-            .approve(router.address(), U256::MAX)
-            .legacy()
-            .send()
-            .await
-            .unwrap();
+        create_pool(
+            token.address(),
+            weth.address(),
+            token_balance_1,
+            eth_balance_1,
+            provider.clone(),
+        )
+        .await;
 
-        let allowance = searcher_token
-            .allowance(deployer, router.address())
-            .call()
-            .await
-            .unwrap();
-        dbg!(allowance);
-
-        let weth_provider = WETH9::new(weth.address(), provider.clone());
-        weth_provider
-            .deposit()
-            .value(eth_balance_1)
-            .legacy()
-            .send()
-            .await
-            .unwrap();
-        let deployer_weth = weth_provider.balance_of(deployer).call().await.unwrap();
-        dbg!(deployer_weth);
-        weth_provider
-            .approve(router.address(), U256::MAX)
-            .legacy()
-            .send()
-            .await
-            .unwrap();
-
-        let searcher_router = UniswapV2Router02::new(router.address(), provider.clone());
-        searcher_router
-            .add_liquidity_eth(
-                token.address(),
-                token_balance_1,
-                U256::zero(),
-                U256::zero(),
-                deployer,
-                time.mul(2u32),
-            )
-            .legacy()
-            .value(eth_balance_1)
-            .send()
-            .await
-            .unwrap();
+        let token_balance_2 = U256::from_dec_str("27402034049012").unwrap();
+        let eth_balance_2 = U256::from_dec_str("8581483325062417688").unwrap();
+        create_pool(
+            token.address(),
+            weth.address(),
+            token_balance_2,
+            eth_balance_2,
+            provider.clone(),
+        )
+        .await;
     }
 }
